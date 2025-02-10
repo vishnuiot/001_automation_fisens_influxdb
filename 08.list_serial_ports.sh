4c7f789d3fe2128a0a324de5993f5d1e3d7df352
vishnuiot <vishnuprasad.iit@gmail.com>
2025-02-10 10:43:18 +0100

    test

---
+++ 08.list_serial_ports.sh
@@ -0,0 +1,2 @@
+# lists all serial ports and attached devices
+sudo dmesg | grep tty
--- 2.get_system_parameters.py
+++ 2.get_system_parameters.py
@@ -1,18 +1,12 @@
-import time,serial
+import time,serial,sys
+import serial
+import time
 
 
-# serialPort = serial.Serial(port = "com3", baudrate=3000000,
-#                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-# serialPort.close()
-# print(serialPort.name)
-# serialPort.close()
-# time.sleep(0.1)  # Delay between communications
 
-# serialPort.open()
-# serialPort.write(b"p?>")
-# x = serialPort.read(1000) 
-# print(x) 
-# print (type(x))
-# print (x.decode())  # removes byte string
-# serialPort.close()
-# time.sleep(0.1)  # Delay between communications
+ser = serial.Serial('/dev/ttyACM0', 3000000)
+time.sleep(2)
+if ser.isOpen():
+    print ("Port Open")
+print(ser.write("START\n")) 
+ser.close()
