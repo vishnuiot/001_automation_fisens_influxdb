324f60c7a66921a520acec4141492dbdf2a0408c
vishnuiot <vishnuprasad.iit@gmail.com>
2025-02-10 11:33:00 +0100

    test

--- 001.get_system_parameters.py
+++ 001.get_system_parameters.py
@@ -1,24 +1,24 @@
 import time,serial,sys
-# import serial
-# import time
 from serial import Serial
-
-
 
 ser = serial.Serial('/dev/ttyUSB0', 3000000)
 time.sleep(2)
 if ser.isOpen():
-    print ("Port Open")
+    print ("Port Open =",ser.name,"\n")
+    ser.write(b"?>")
+    device_model = ser.read(15) 
+    print(device_model)
+    print (type(device_model))
+    print (device_model.decode())  # removes byte string   
 ser.close()
 
-ser = serial.Serial('/dev/ttyUSB0', 3000000)
-# print(serialPort.name)
-# serialPort.close()
-# time.sleep(0.1)  # Delay between communications
-# serialPort.open()
-# serialPort.write(b"?>")
+# ser = serial.Serial('/dev/ttyUSB0', 3000000)
+# print("information on the serial port =",ser,"\n")
+# # ser.close()
+# # serial.open()
+# serial.write(b"?>")
 # x = serialPort.read(100) 
 # print(x) 
-# serialPort.close()
+# ser.close()
 # time.sleep(0.1)  # Delay between communications
 
